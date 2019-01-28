b5b16c3da5b75c5b218c30b66df9c52c8e9e8129
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2019-01-27 19:40:23 -0600

    Seção 1, Aula 12 - Tabela Lista de Artigos

--- blog/resources/views/admin/artigos/index.blade.php
+++ blog/resources/views/admin/artigos/index.blade.php
@@ -3,7 +3,66 @@
 @section('content')
     <pagina tamanho='12'>
         <painel titulo='Lista de artigos'>
-            Teste de Conteudo...
+            <a href='#'>Criar</a>
+
+            <table class="table table-bordered table-hover table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Titulo</th>
+                        <th>Descrição</th>
+                        <th>Autor</th>
+                        <th>Data</th>
+                        <th>Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Titulo</td>
+                        <td>Descrição</td>
+                        <td>Autor</td>
+                        <td>Data</td>
+                        <td>
+                            <a href='#'>Editar</a> | 
+                            <a href='#'>Deletar</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Titulo</td>
+                        <td>Descrição</td>
+                        <td>Autor</td>
+                        <td>Data</td>
+                        <td>
+                            <a href='#'>Editar</a> | 
+                            <a href='#'>Deletar</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Titulo</td>
+                        <td>Descrição</td>
+                        <td>Autor</td>
+                        <td>Data</td>
+                        <td>
+                            <a href='#'>Editar</a> | 
+                            <a href='#'>Deletar</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Titulo</td>
+                        <td>Descrição</td>
+                        <td>Autor</td>
+                        <td>Data</td>
+                        <td>
+                            <a href='#'>Editar</a> | 
+                            <a href='#'>Deletar</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </painel>
     </pagina>
 @endsection
