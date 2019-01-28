1f03aa51b4b6edcee42a6894af6ea206fc71babd
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2019-01-27 19:33:54 -0600

    Seção 1, Aula 11 - Criando a View para Lista de Artigos

---
+++ blog/resources/views/admin/artigos/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <pagina tamanho='12'>
+        <painel titulo='Lista de artigos'>
+            Teste de Conteudo...
+        </painel>
+    </pagina>
+@endsection
