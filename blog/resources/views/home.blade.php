dec252c06f2bf809cf06bd84e49cc492a37810e4
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2019-01-27 19:14:39 -0600

    Seção 1, Aula 9 - Componente Pagina

--- blog/resources/views/home.blade.php
+++ blog/resources/views/home.blade.php
@@ -1,31 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <painel titulo='Dashboard'>
-                    Teste de conteúdo
+    <pagina tamanho='10'>
+        <painel titulo='Dashboard'>
+            Teste de conteúdo
 
-                    <div class='row'>
-                        <div class='col-md-4'>
-                            <caixa qtd='80' titulo='Artigos' url='#' cor='orange' icone='ion ion-pie-graph'>
-                                Teste de conteúdo
-                            </caixa>
-                        </div>
-                        <div class='col-md-4'>
-                            <caixa qtd='1500' titulo='Usuários' url='#' cor='blue' icone='ion ion-person-stalker'>
-                                Teste de conteúdo
-                            </caixa>
-                        </div>
-                        <div class='col-md-4'>
-                            <caixa qtd='3' titulo='Autores' url='#' cor='red' icone='ion ion-person'>
-                                Teste de conteúdo
-                            </caixa>
-                        </div>
-                    </div>
-                </painel>
+            <div class='row'>
+                <div class='col-md-4'>
+                    <caixa qtd='80' titulo='Artigos' url='#' cor='orange' icone='ion ion-pie-graph'>
+                        Teste de conteúdo
+                    </caixa>
+                </div>
+                <div class='col-md-4'>
+                    <caixa qtd='1500' titulo='Usuários' url='#' cor='blue' icone='ion ion-person-stalker'>
+                        Teste de conteúdo
+                    </caixa>
+                </div>
+                <div class='col-md-4'>
+                    <caixa qtd='3' titulo='Autores' url='#' cor='red' icone='ion ion-person'>
+                        Teste de conteúdo
+                    </caixa>
+                </div>
             </div>
-        </div>
-    </div>
+        </painel>
+    </pagina>
 @endsection
