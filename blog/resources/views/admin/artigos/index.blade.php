043f45036868ff14b31477f885e5c7db01e30bee
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2019-02-18 13:57:25 -0600

    Seção 1, Aula 25 - Componente de Modal

--- blog/resources/views/admin/artigos/index.blade.php
+++ blog/resources/views/admin/artigos/index.blade.php
@@ -5,6 +5,8 @@
         <painel titulo='Lista de artigos'>
             <migalhas v-bind:lista='{{$listaMigalhas}}'></migalhas>
 
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#meuModalTeste">Large modal</button>
+
             <tabela-lista 
                 v-bind:titulos="['#', 'Titulo', 'Descrição']"
                 v-bind:items="[
@@ -21,4 +23,30 @@
             ></tabela-lista>
         </painel>
     </pagina>
+
+    <modal nome='meuModalTeste'>
+        <painel titulo='Adicionar'>
+            <form>
+                <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                </div>
+                <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="form-group">
+                <label for="exampleInputFile">File input</label>
+                <input type="file" id="exampleInputFile">
+                <p class="help-block">Example block-level help text here.</p>
+                </div>
+                <div class="checkbox">
+                <label>
+                    <input type="checkbox"> Check me out
+                </label>
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+        </painel>
+    </modal>
 @endsection
