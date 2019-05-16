9065ddf27f273695a0432d98ee5206879c45e241
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2019-05-16 13:26:08 -0600

    Seção 1, Aula 59 - Página Detalhe do Artigo

--- blog/resources/views/site.blade.php
+++ blog/resources/views/site.blade.php
@@ -8,7 +8,7 @@
                     <artigocard 
                         titulo='{{$artigo->titulo}}'
                         descricao='{{$artigo->descricao}}'
-                        link='#'
+                        link='{{route('artigo', [$artigo->id, str_slug($artigo->titulo, '-')])}}'
                         imagem='https://f.i.uol.com.br/fotografia/2019/03/15/15526795065c8c025270c53_1552679506_4x3_lg.jpg'
                         data='{{$artigo->data}}'
                         autor='{{$artigo->autor}}'
